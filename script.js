523b06a3faf649d5aaff0e4b41a51804a7dcaf47
pepper-0 <143720331+pepper-0@users.noreply.github.com>
2025-08-10 22:41:09 -0400

    moved bio information around + added translation intro

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 /* .js files add interaction to your website */
+var translateIndex = 0;
+var welcomeText = [
+    "⋆.˚✮🎧✮˚.⋆  hello! i'm jana leung, welcome to my corner :\)\nfeel free to look around, and relax\!\nthis is a personal website, so it doesn't have any specific theme or purpose\.\n\(pssst, if you want more information/to contact me, you can click my logo...)",
+    "⋆.˚✮🎧✮˚.⋆  你好! 我叫梁靜悠，欢迎来到我的网站 :)"
+]
 
+var translateText = document.getElementById("translatable");
+var translateButton = document.getElementById("translateButton");
+
+translateButton.addEventListener("click", changeLanguage);
+
+function changeLanguage() {
+    translateIndex++;
+    if (translateIndex == welcomeText.length) {
+        translateIndex = 0;
+    }
+    translateText.innerHTML = welcomeText[translateIndex];
+}
