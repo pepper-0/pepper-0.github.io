1f1b35d498aa184ba2e0c8243ef29db09feff304
pepper-0 <143720331+pepper-0@users.noreply.github.com>
2025-08-16 23:19:25 -0400

    retreating back to old formatting prior to making arrays for list/icon switching

--- fiber_arts_script.js
+++ fiber_arts_script.js
@@ -7,89 +7,63 @@ var currentView = 0;
 listView.addEventListener("click", switchToList);
 iconView.addEventListener("click", switchToIcon);
 
-var iconExampleCard = document.getElementById("exampleCard");
+var exampleCard = document.getElementById("exampleCard");
 
-var iconProjectCards = document.getElementsByClassName("projectCard");
-var iconProjectNames = document.getElementsByClassName("projectName");
-var iconProjectDates = document.getElementsByClassName("projectDate");
-var iconProjectDescs = document.getElementsByClassName("projectDesc");
+var projectCards = document.getElementsByClassName("projectCard");
+var projectNames = document.getElementsByClassName("projectName");
+var projectDates = document.getElementsByClassName("projectDate");
+var projectDescs = document.getElementsByClassName("projectDesc");
 
-var iconPortfolioImgs = document.getElementsByClassName("portfolioImg");
-var iconLinks = document.getElementsByClassName("link");
-
-// create copies of original elemetns for list view
-var listExampleCard = iconExampleCard;
-var listProjectCards = iconProjectCards;
-var listProjectNames = iconProjectNames;
-var listProjectDates = iconProjectDates;
-var listProjectDescs = iconProjectDescs;
-var listPortfolioImgs = iconPortfolioImgs;
-var listLinks = iconLinks;
-
-// now modify the original elements for list view
-for (var i = 0; i < listProjectCards.length; i++) {
-    listProjectCards[i].style.maxHeight = "50px";
-//    listProjectCards[i].style.display = "flex";
-    listProjectCards[i].style.flexDirection = "row";
-
-    // reset margins and padding
-    listProjectNames[i].style.padding = "0";
-    listProjectDescs[i].style.padding = "0";
-    listPortfolioImgs[i].style.margin = "0";
-
-    // space ratios
-    listProjectNames[i].style.width = "15%";
-    listProjectNames[i].style.marginRight = "2.5%";
-    listProjectNames[i].style.marginLeft = "2.5%";
-    listProjectDescs[i].style.width = "55%";
-    listProjectDescs[i].style.marginRight = "2.5%";
-    listProjectDescs[i].style.marginLeft = "2.5%";
-    listPortfolioImgs[i].style.width = "15%";
-    listPortfolioImgs[i].style.marginRight = "2.5%";
-    listPortfolioImgs[i].style.marginLeft = "2.5%";
-
-    // title
-    listProjectNames[i].style.font = "1.0em 'Monaco', monospace";
-    listProjectNames[i].style.fontWeight = "bold";
-    listProjectNames[i].style.textShadow = "none";     
-
-    // images
-    listPortfolioImgs[i].style.maxHeight = "100%";
-    listPortfolioImgs[i].style.marginLeft = "auto";
-    listPortfolioImgs[i].style.objectFit = "cover";
-
-    // descs
-    listProjectDescs[i].style.overflow = "hidden";
-    listProjectDescs[i].style.textOverflow = "ellipsis";
-    listProjectDescs[i].style.whiteSpace = "nowrap";
-}
-switchToIcon(); // default view is icon view
-
+var portfolioImgs = document.getElementsByClassName("portfolioImg");
+var links = document.getElementsByClassName("link");
 
 function switchToList() {
     currentView = 1;
     console.log("Switching to list view");
-    exampleCard.style.display = "block"; 
-    for (var i = 0; i < listProjectCards.length; i++) {
-        iconExampleCard.style.display = "none"; // hide the example card in icon view
-        iconProjectCards[i].style.display = "none"; 
-        iconProjectNames[i].style.display = "none";
-        iconProjectDates[i].style.display = "none";
-        iconProjectDescs[i].style.display = "none";
-        iconPortfolioImgs[i].style.display = "none";
-        iconLinks[i].style.display = "none";
+    exampleCard.style.display = "block";
+    for (var i = 0; i < projectCards.length; i++) {
+        projectCards[i].style.maxHeight = "50px";
+        projectCards[i].style.display = "flex";
+        projectCards[i].style.flexDirection = "row";
 
-        // show list view 
-        listExampleCard.style.display = "block";
-        listProjectCards[i].style.display = "flex"; // show the project cards in list view
-        listProjectNames[i].style.display = "inline-block";
-        listProjectDates[i].style.display = "none";
-        listProjectDescs[i].style.display = "inline-block";
-        listPortfolioImgs[i].style.display = "inline-block";
-        listLinks[i].style.display = "inline-block";
-        listProjectCards[i].style.height = "auto"; // set height to auto for list view
+        // remove date and links
+        projectDates[i].style.display = "none";
+        if (links[i]) {
+            links[i].style.display = "none";
+        }
 
+        // reset margins and padding
+        projectNames[i].style.padding = "0";
+        projectDescs[i].style.padding = "0";
+        portfolioImgs[i].style.margin = "0";
 
+        // space ratios
+        projectNames[i].style.width = "15%";
+        projectNames[i].style.marginRight = "2.5%";
+        projectNames[i].style.marginLeft = "2.5%";
+        projectDescs[i].style.width = "55%";
+        projectDescs[i].style.marginRight = "2.5%";
+        projectDescs[i].style.marginLeft = "2.5%";
+        portfolioImgs[i].style.width = "15%";
+        portfolioImgs[i].style.marginRight = "2.5%";
+        portfolioImgs[i].style.marginLeft = "2.5%";
+
+        // title
+        projectNames[i].style.font = "1.0em 'Monaco', monospace";
+        projectNames[i].style.fontWeight = "bold";
+        projectNames[i].style.textShadow = "none";     
+
+        // images
+        portfolioImgs[i].style.maxHeight = "100%";
+        portfolioImgs[i].style.marginLeft = "auto";
+        portfolioImgs[i].style.objectFit = "cover";
+
+        // descs
+        projectDescs[i].style.overflow = "hidden";
+        projectDescs[i].style.textOverflow = "ellipsis";
+        projectDescs[i].style.whiteSpace = "nowrap";
+    
+        
     }
     
 
@@ -101,63 +75,46 @@ function switchToIcon() {
     currentView = 0;
 
     for (var i = 0; i < projectCards.length; i++) {
-        iconExampleCard.style.display = "block"; // hide the example card in icon view
-        iconProjectCards[i].style.display = "block"; 
-        iconProjectNames[i].style.display = "block";
-        iconProjectDates[i].style.display = "block";
-        iconProjectDescs[i].style.display = "block";
-        iconPortfolioImgs[i].style.display = "block";
-        iconLinks[i].style.display = "block";
-
-        // show list view 
-        listExampleCard.style.display = "block";
-        listProjectCards[i].style.display = "flex"; // show the project cards in list view
-        listProjectNames[i].style.display = "inline-block";
-        listProjectDates[i].style.display = "none";
-        listProjectDescs[i].style.display = "inline-block";
-        listPortfolioImgs[i].style.display = "inline-block";
-        listLinks[i].style.display = "inline-block";
-        listProjectCards[i].style.height = "auto"; // set height to auto for list view        
-        // projectCards[i].style.height = "auto";
-        // projectCards[i].style.display = "inline-block";
+        projectCards[i].style.height = "auto";
+        projectCards[i].style.display = "inline-block";
 
 
-        // // title
-        // projectNames[i].style.width = "100%";
-        // projectNames[i].style.font = "1.2em 'TKindred', sans-serif";
-        // projectNames[i].style.fontWeight = "bold";
-        // projectNames[i].style.textShadow = "2px 2px #F4F1DE";    
-        // projectNames[i].style.display = "inline-block";
+        // title
+        projectNames[i].style.width = "100%";
+        projectNames[i].style.font = "1.2em 'TKindred', sans-serif";
+        projectNames[i].style.fontWeight = "bold";
+        projectNames[i].style.textShadow = "2px 2px #F4F1DE";    
+        projectNames[i].style.display = "inline-block";
 
 
-        // // add date and links
-        // projectDates[i].style.display = "inline-block";
-        // projectDates[i].style.textAlign = "left";
-        // if (links[i]) {
-        //     links[i].style.display = "inline-block";
-        //     links[i].style.margin = "10px auto";
-        // }
+        // add date and links
+        projectDates[i].style.display = "inline-block";
+        projectDates[i].style.textAlign = "left";
+        if (links[i]) {
+            links[i].style.display = "inline-block";
+            links[i].style.margin = "10px auto";
+        }
 
-        // // reset margins and padding
-        // projectNames[i].style.padding = "10px";
-        // projectDescs[i].style.padding = "10px";
-        // portfolioImgs[i].style.margin = "10px";
+        // reset margins and padding
+        projectNames[i].style.padding = "10px";
+        projectDescs[i].style.padding = "10px";
+        portfolioImgs[i].style.margin = "10px";
 
-        // // descriptions
-        // projectDescs[i].style.height = "auto";
-        // projectDescs[i].style.width = "auto";
-        // projectDescs[i].style.overflow = "normal";
-        // projectDescs[i].style.textOverflow = "clip";
-        // projectDescs[i].style.whiteSpace = "normal";
-        // projectDescs[i].style.textAlign = "left";
+        // descriptions
+        projectDescs[i].style.height = "auto";
+        projectDescs[i].style.width = "auto";
+        projectDescs[i].style.overflow = "normal";
+        projectDescs[i].style.textOverflow = "clip";
+        projectDescs[i].style.whiteSpace = "normal";
+        projectDescs[i].style.textAlign = "left";
 
 
-        // // images
-        // portfolioImgs[i].style.display = "inline-block";
-        // portfolioImgs[i].style.width = "80%";
-        // portfolioImgs[i].style.margin = "10px auto";
-        // portfolioImgs[i].style.height = "auto";
-        // portfolioImgs[i].style.maxHeight = "500px";  
+        // images
+        portfolioImgs[i].style.display = "inline-block";
+        portfolioImgs[i].style.width = "80%";
+        portfolioImgs[i].style.margin = "10px auto";
+        portfolioImgs[i].style.height = "auto";
+        portfolioImgs[i].style.maxHeight = "500px";  
 
     }
 }
